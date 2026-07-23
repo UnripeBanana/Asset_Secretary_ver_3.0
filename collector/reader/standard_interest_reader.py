60aa5dea78d98cda4c6a619d96681a318d31df0c
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-23 12:44:59 +0900

    Update standard_interest_reader.py

--- collector/reader/standard_interest_reader.py
+++ collector/reader/standard_interest_reader.py
@@ -1,1 +1,5 @@
-
+# https://m.stock.naver.com/front-api/marketIndex/standardInterest?category=standardInterest&reutersCode=USA&page=1
+"""
+- Federal Funds Rate (Fed Rate)
+- Bank of Korea Base Rate
+"""
