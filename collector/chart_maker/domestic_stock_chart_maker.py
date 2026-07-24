52a5d4b5c0c418c390e392d069ff60192cd944bf
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-24 16:13:29 +0900

    Update domestic_stock_chart_maker.py

--- collector/chart_maker/domestic_stock_chart_maker.py
+++ collector/chart_maker/domestic_stock_chart_maker.py
@@ -168,7 +168,48 @@ for page in get_all_pages(NOTION_DOMESTIC_STOCK_INFO_DB_ID):
     #-----------------------------------------------------
     # 최고가 최저가 표시
     #-----------------------------------------------------
-    present_high_and_low (ax, stock)
+    # 최고가 / 최저가
+    high_idx = stock["high"].idxmax()
+    low_idx = stock["low"].idxmin()
+    
+    high_price = stock.loc[high_idx, "high"]
+    low_price = stock.loc[low_idx, "low"]
+    
+    # 최고가 표시
+    ax.plot(
+        high_idx,
+        high_price + 700,
+        marker="v",                          # ▼ 표시
+        color="gray",
+        markersize=5
+    )
+    ax.text(
+        high_idx + 4,                      # 왼쪽으로 약간 이동
+        high_price + 700,          # ▼와 같은 높이
+        f"High Price {high_price:,}",
+        va="center",
+        ha="right",
+        fontsize=8,
+        color="gray"
+    )
+    
+    # 최저가 표시
+    ax.plot(
+        low_idx,
+        low_price - 700,
+        marker="^",                          # ▲ 표시
+        color="gray",
+        markersize=5
+    )
+    ax.text(
+        low_idx + 0.5,                       # 오른쪽으로 약간 이동
+        low_price - 700,           # ▲와 같은 높이
+        f"Low Price {low_price:,}",
+        va="center",
+        ha="left",
+        fontsize=8,
+        color="gray"
+    )
 
     #-----------------------------------------------------
     # 현재가 표시
