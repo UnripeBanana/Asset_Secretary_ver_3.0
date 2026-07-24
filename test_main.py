70ca8d62637b6a9b2ff65128afae6a2c3b91c054
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-24 16:55:03 +0900

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,12 +1,11 @@
 from collector.data_reader.domestic_stock_data_reader import domestic_stock_data_reader
-from collector.data_reader.index_data_reader import index_data_reader
-from collector.data_reader.price_data_reader import price_data_reader
-from collector.data_reader.standard_interest_data_reader import standard_interest_data_reader
+#from collector.data_reader.index_data_reader import index_data_reader
+#from collector.data_reader.price_data_reader import price_data_reader
+#from collector.data_reader.standard_interest_data_reader import standard_interest_data_reader
 
-from collector.data_processor.domestic_stock_data_processor import domestic_stock_data_processor
-from collector.data_processor.index_data_processor import index_data_processor
-from collector.data_processor.price_data_processor import price_data_processor
-from collector.data_processor.standard_interest_data_processor import standard_interest_data_processor
+#from collector.data_processor.index_data_processor import index_data_processor
+#from collector.data_processor.price_data_processor import price_data_processor
+#from collector.data_processor.standard_interest_data_processor import standard_interest_data_processor
 
 #from collector.chart_maker.domestic_stock_chart_maker import domestic_stock_chart_maker
 #from collector.chart_maker.index_chart_maker import index_chart_maker
@@ -14,4 +13,5 @@ from collector.data_processor.standard_interest_data_processor import standard_i
 #from collector.chart_maker.standard_interest_chart_maker import standard_interest_chart_maker
 
 
-print(domestic_stock_data_reader(20250720, 20260724, "005930"))
+print(domestic_stock_data_reader(20250720, 20260724, "005930").iterrows())
+
