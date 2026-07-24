92597b4a0686e08204cd1a6694d50dd9f12aa09f
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-24 16:11:04 +0900

    Update domestic_stock_chart_maker.py

--- collector/chart_maker/domestic_stock_chart_maker.py
+++ collector/chart_maker/domestic_stock_chart_maker.py
@@ -48,7 +48,17 @@ for page in get_all_pages(NOTION_DOMESTIC_STOCK_INFO_DB_ID):
     make_candle_chart(ax, stock)
 
     # 이동평균선
-    moving_average(ax, stock, x)
+    stock["MA5"] = stock["close"].rolling(5).mean()
+    stock["MA20"] = stock["close"].rolling(20).mean()
+    stock["MA60"] = stock["close"].rolling(60).mean()
+    stock["MA120"] = stock["close"].rolling(120).mean()
+    
+    ax.plot(x, stock["MA5"], color="orange", linewidth=1.2, label="5")
+    ax.plot(x, stock["MA20"], color="red", linewidth=1.2, label="20")
+    ax.plot(x, stock["MA60"], color="green", linewidth=1.2, label="60")
+    ax.plot(x, stock["MA120"], color="blue", linewidth=1.2, label="120")
+    
+    ax.legend(loc="upper left")
 
     # 축 설정
     set_axis(ax, stock)
