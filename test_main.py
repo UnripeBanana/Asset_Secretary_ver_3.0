aee75349f1a02d22261336c0ab8a3b6766372060
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-24 14:59:55 +0900

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,1 +1,14 @@
+from collector.reader.domestic_stock_reader import
+from collector.reader.index_reader import
+from collector.reader.price_reader import
+from collector.reader.standard_interest_reader import
 
+from collector.processor.domestic_stock_reader import
+from collector.processor.index_reader import
+from collector.processor.price_reader import
+from collector.processor.standard_interest_reader import
+
+from collector.maker.domestic_stock_reader import
+from collector.maker.index_reader import
+from collector.maker.price_reader import
+from collector.maker.standard_interest_reader import
