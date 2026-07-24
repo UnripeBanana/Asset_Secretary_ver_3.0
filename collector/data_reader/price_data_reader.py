f6de6993f45f20be49fefa8b13c547a32f7176da
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-24 15:06:36 +0900

    Update price_data_reader.py

--- collector/data_reader/price_data_reader.py
+++ collector/data_reader/price_data_reader.py
@@ -22,7 +22,7 @@ import requests
 import pandas as pd
 from data_ver2.market_index.naver_to_df import make_market_index_df
 
-def market_index_reader(start, end, category, ticker, name, currency):
+def price_data_reader(start, end, category, ticker, name, currency):
     start = pd.to_datetime(str(start), format="%Y%m%d")
     end = pd.to_datetime(str(end), format="%Y%m%d")
 
