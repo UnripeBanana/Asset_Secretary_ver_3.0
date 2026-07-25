1cf0149d86dbb4df35e4d29430d6e26839a0c18e
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-25 20:08:50 +0900

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,10 +1,10 @@
 from collector.data_reader.domestic_stock_data_reader import domestic_stock_data_reader
-#from collector.data_reader.index_data_reader import index_data_reader
+from collector.data_reader.index_data_reader import index_data_reader
 #from collector.data_reader.price_data_reader import price_data_reader
 #from collector.data_reader.standard_interest_data_reader import standard_interest_data_reader
 
 from collector.chart_maker.domestic_stock_chart_maker import domestic_stock_chart_maker
-#from collector.chart_maker.index_chart_maker import index_chart_maker
+from collector.chart_maker.index_chart_maker import index_chart_maker
 #from collector.chart_maker.price_chart_maker import price_chart_maker
 #from collector.chart_maker.standard_interest_chart_maker import standard_interest_chart_maker
 
@@ -14,4 +14,4 @@ domestic_stock_chart_maker(domestic_stock_data_reader("2025-07-20", "2026-07-24"
 
 
 # 작업 중
-#print(index_data_reader("2025-07-20", "2026-07-24", "KOSPI"))
+index_chart_maker(index_data_reader("2025-07-20", "2026-07-24", "KOSPI"))
