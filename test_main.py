5c4001cc1cf93aff8db5853659a80bd9f57ceb05
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-25 15:24:10 +0900

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,17 +1,17 @@
-#from collector.data_reader.domestic_stock_data_reader import domestic_stock_data_reader
-from collector.data_reader.index_data_reader import index_data_reader
+from collector.data_reader.domestic_stock_data_reader import domestic_stock_data_reader
+#from collector.data_reader.index_data_reader import index_data_reader
 #from collector.data_reader.price_data_reader import price_data_reader
 #from collector.data_reader.standard_interest_data_reader import standard_interest_data_reader
 
-#from collector.chart_maker.domestic_stock_chart_maker import domestic_stock_chart_maker
+from collector.chart_maker.domestic_stock_chart_maker import domestic_stock_chart_maker
 #from collector.chart_maker.index_chart_maker import index_chart_maker
 #from collector.chart_maker.price_chart_maker import price_chart_maker
 #from collector.chart_maker.standard_interest_chart_maker import standard_interest_chart_maker
 
 
 # 작업 완료
-#domestic_stock_chart_maker(domestic_stock_data_reader(2025-07-20, 2026-07-24, "005930"), "삼성전자")
+domestic_stock_chart_maker(domestic_stock_data_reader(2025-07-20, 2026-07-24, "005930"), "삼성전자")
 
 
 # 작업 중
-print(index_data_reader("2025-07-20", "2026-07-24", "KOSPI"))
+#print(index_data_reader("2025-07-20", "2026-07-24", "KOSPI"))
