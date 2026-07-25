0bd630e0562612a877b1c570bbdf91dcf7f0e677
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-25 13:12:28 +0900

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -7,7 +7,7 @@ from collector.data_reader.domestic_stock_data_reader import domestic_stock_data
 #from collector.data_processor.price_data_processor import price_data_processor
 #from collector.data_processor.standard_interest_data_processor import standard_interest_data_processor
 
-#from collector.chart_maker.domestic_stock_chart_maker import domestic_stock_chart_maker
+from collector.chart_maker.domestic_stock_chart_maker import domestic_stock_chart_maker
 #from collector.chart_maker.index_chart_maker import index_chart_maker
 #from collector.chart_maker.price_chart_maker import price_chart_maker
 #from collector.chart_maker.standard_interest_chart_maker import standard_interest_chart_maker
