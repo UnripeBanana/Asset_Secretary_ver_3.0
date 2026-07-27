5a186f6624e6b4138b53707e6a3aa7768f439252
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-27 16:47:13 +0900

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -13,19 +13,21 @@ from collector.chart_maker.price_chart_maker import price_chart_maker
 #-------------------------------------------
 # 작업 완료
 #-------------------------------------------
-domestic_stock_chart_maker(domestic_stock_data_reader("2025-07-20", "2026-07-27", "005930"), "삼성전자")
-domestic_stock_candle_chart_maker(domestic_stock_data_reader("2025-07-20", "2026-07-27", "005930"), "삼성전자")
+#domestic_stock_chart_maker(domestic_stock_data_reader("2025-07-20", "2026-07-27", "005930"), "삼성전자")
+#domestic_stock_candle_chart_maker(domestic_stock_data_reader("2025-07-20", "2026-07-27", "005930"), "삼성전자")
 
-index_chart_maker(index_data_reader("2025-07-20", "2026-07-27", "KOSPI"))                
+#index_chart_maker(index_data_reader("2025-07-20", "2026-07-27", "KOSPI"))                
 # 입력 가능한 항목 : "KOSPI", "KOSDAQ", "KOSPI_200", "NASDAQ", "S&P_500", "Dow_Jones", "VIX"
 
-index_candle_chart_maker(index_data_reader("2025-07-20", "2026-07-27", "KOSPI"))         
+#index_candle_chart_maker(index_data_reader("2025-07-20", "2026-07-27", "KOSPI"))         
 # 입력 가능한 항목 : "KOSPI", "KOSDAQ", "KOSPI_200", "NASDAQ", "S&P_500", "Dow_Jones", "VIX"
+
+price_chart_maker(price_data_reader("2025-07-20", "2026-07-27", "KRW_Gold"))              
+# 입력 가능한 항목 : "US2Y", "US10Y", "US30Y", "KR3Y", "KR10Y", "KR30Y", "USD/KRW", "Dolar_Index", "USD/JPY", "USD/EUR", 
+#                   "KRW_Gold", "International_Gold", "Silver", "WTI_Crude_Oil", "Brent_Crude_Oil", "Natural_Gas", "Copper"
 
 
 #-------------------------------------------
 # 작업 중
 #-------------------------------------------
-price_chart_maker(price_data_reader("2026-07-20", "2026-07-27", "KRW_Gold"))              
-# 입력 가능한 항목 : "US2Y", "US10Y", "US30Y", "KR3Y", "KR10Y", "KR30Y", "USD/KRW", "Dolar_Index", "USD/JPY", "USD/EUR", 
-#                   "KRW_Gold", "International_Gold", "Silver", "WTI_Crude_Oil", "Brent_Crude_Oil", "Natural_Gas", "Copper"
+
