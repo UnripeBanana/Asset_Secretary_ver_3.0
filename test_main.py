4c459de70abd9ef446dd8b4c3479424f029e1949
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-27 14:20:58 +0900

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,7 +3,7 @@ from collector.data_reader.index_data_reader import index_data_reader
 #from collector.data_reader.price_data_reader import price_data_reader
 #from collector.data_reader.standard_interest_data_reader import standard_interest_data_reader
 
-#from collector.chart_maker.domestic_stock_chart_maker import domestic_stock_chart_maker
+from collector.chart_maker.domestic_stock_chart_maker import domestic_stock_chart_maker
 from collector.chart_maker.domestic_stock_candle_chart_maker import domestic_stock_candle_chart_maker
 from collector.chart_maker.index_chart_maker import index_chart_maker
 from collector.chart_maker.index_candle_chart_maker import index_candle_chart_maker
@@ -19,4 +19,4 @@ from collector.chart_maker.index_candle_chart_maker import index_candle_chart_ma
 
 
 # 작업 중
-#domestic_stock_chart_maker(domestic_stock_data_reader("2025-07-20", "2026-07-24", "005930"), "삼성전자")
+domestic_stock_chart_maker(domestic_stock_data_reader("2025-07-20", "2026-07-24", "005930"), "삼성전자")
