03196048bc1b1916d4e800e7987e9f1d64abb487
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-27 13:34:39 +0900

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -4,9 +4,9 @@ from collector.data_reader.index_data_reader import index_data_reader
 #from collector.data_reader.standard_interest_data_reader import standard_interest_data_reader
 
 #from collector.chart_maker.domestic_stock_chart_maker import domestic_stock_chart_maker
-from collector.chart_maker.domestic_stock_chart_maker import domestic_stock_candle_chart_maker
+from collector.chart_maker.domestic_stock_candle_chart_maker import domestic_stock_candle_chart_maker
 from collector.chart_maker.index_chart_maker import index_chart_maker
-from collector.chart_maker.index_chart_maker import index_candle_chart_maker
+from collector.chart_maker.index_candle_chart_maker import index_candle_chart_maker
 #from collector.chart_maker.price_chart_maker import price_chart_maker
 #from collector.chart_maker.standard_interest_chart_maker import standard_interest_chart_maker
 
