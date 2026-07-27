36c908feabe9827094f827e60d1c698fa10cfac1
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-27 16:15:09 +0900

    Update price_data_reader.py

--- collector/data_reader/price_data_reader.py
+++ collector/data_reader/price_data_reader.py
@@ -20,20 +20,72 @@
 
 import requests
 import pandas as pd
-from collector.data_processor.price_data_processor import price_data_processor
+#from collector.data_processor.price_data_processor import price_data_processor
 
-def price_data_reader(start, end, category, ticker, name, currency):
-    start = pd.to_datetime(str(start), format="%Y%m%d")
-    end = pd.to_datetime(str(end), format="%Y%m%d")
+def price_data_reader(start, end, code):
+    start = pd.to_datetime(str(start))
+    end = pd.to_datetime(str(end))
 
     page = 1
     dfs = []
+
+    code_trans = {
+        "US2Y": "US2YT%3DRR",
+        "US10Y": "US10YT%3DRR",
+        "US30Y": "US30YT%3DRR",
+        "KR3Y": "KR3YT%3DRR",
+        "KR10Y": "KR10YT%3DRR",
+        "KR30Y": "KR30YT%3DRR",
+        "USD/KRW": "FX_USDKRW",
+        "Dolar_Index": ".DXY",
+        "USD/JPY": "USDJPY",
+        "USD/EUR": "USDEUR",
+        "KRW_Gold": "M04020000",
+        "International_Gold": "GCcv1",
+        "Silver": "SIcv1",
+        "WTI_Crude_Oil": "CLcv1",
+        "Brent_Crude_Oil": "LCOcv1",
+        "Natural_Gas": "NGcv1",
+        "Copper": "CMCU0"
+    }
+
+    code = code_trans[code]
+
+    if code in ["US2YT%3DRR", "US10YT%3DRR", "US30YT%3DRR", "KR3YT%3DRR", "KR10YT%3DRR", "KR30YT%3DRR"]:
+        category = "bond"
+    elif code in ["M04020000", "GCcv1", "SIcv1", "CLcv1", "LCOcv1", "NGcv1", "CMCU0"]:
+        category = "metals"
+    elif code in ["FX_USDKRW", ".DXY", "USDJPY", "USDEUR"]:
+        category = "exchange"
+    else:
+        raise ValueError(f"Invalid code: {code}")
+
+    currency_list = {
+        "US2YT%3DRR": "%", 
+        "US10YT%3DRR": "%", 
+        "US30YT%3DRR": "%", 
+        "KR3YT%3DRR": "%", 
+        "KR10YT%3DRR": "%", 
+        "KR30YT%3DRR": "%",
+        "M04020000": "원/g",
+        "GCcv1": "USD/OZS",
+        "SIcv1": "USD/OZS",
+        "CLcv1": "USD/BBL",
+        "LCOcv1": "USD/BBL",
+        "NGcv1": "USD/MMBTU",
+        "CMCU0": "USD/TONNE",
+        "FX_USDKRW": "USD/KRW",
+        ".DXY": "-",
+        "USDJPY": "USD/JPY",
+        "USDEUR": "USD/EUR"
+    }
+
     
     while True:
         url = (
             "https://m.stock.naver.com/front-api/marketIndex/prices"
             f"?category={category}"
-            f"&reutersCode={ticker}"
+            f"&reutersCode={code}"
             f"&page={page}"
         )
         
@@ -49,7 +101,7 @@ def price_data_reader(start, end, category, ticker, name, currency):
         if not price_data.get("result"):
             break    
 
-        page_df = make_market_index_df(price_data, ticker, name)
+        page_df = price_data_processor(price_data, code)
 
         page_df["date"] = (
             pd.to_datetime(page_df["date"], utc=True)
@@ -69,7 +121,7 @@ def price_data_reader(start, end, category, ticker, name, currency):
 
     price_data = pd.concat(dfs, ignore_index=True)
 
-    price_data["currency"] = currency
+    price_data["currency"] = currency_list[code]
 
     price_data = price_data[
         (price_data["date"] >= start) &
