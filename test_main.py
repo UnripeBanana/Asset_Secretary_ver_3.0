6718071c109304290af2c1481b36db7cfe3c933e
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-27 16:35:23 +0900

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -26,6 +26,6 @@ index_candle_chart_maker(index_data_reader("2025-07-20", "2026-07-27", "KOSPI"))
 #-------------------------------------------
 # 작업 중
 #-------------------------------------------
-price_chart_maker(price_data_reader("2025-07-20", "2026-07-27", "KRW_Gold"))              
+price_chart_maker(price_data_reader("2026-07-20", "2026-07-27", "KRW_Gold"))              
 # 입력 가능한 항목 : "US2Y", "US10Y", "US30Y", "KR3Y", "KR10Y", "KR30Y", "USD/KRW", "Dolar_Index", "USD/JPY", "USD/EUR", 
 #                   "KRW_Gold", "International_Gold", "Silver", "WTI_Crude_Oil", "Brent_Crude_Oil", "Natural_Gas", "Copper"
