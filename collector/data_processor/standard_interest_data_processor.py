62013eac6506f8e6bb727366db36133860c397d5
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-28 12:32:56 +0900

    Update standard_interest_data_processor.py

--- collector/data_processor/standard_interest_data_processor.py
+++ collector/data_processor/standard_interest_data_processor.py
@@ -1,45 +1,45 @@
 import pandas as pd
 
-def price_data_processor(data, code):
+def standard_interest_data_processor(data, code):
 
     # 네이버 증권에서 받은 오리지널 데이터
-    price_df = pd.DataFrame(data["result"])
+    standard_interest_df = pd.DataFrame(data["result"])
 
     # 오리지널 데이터에서 불필요한 부분 제거
-    price_df = price_df[["localTradedAt", "closePrice", "fluctuations"]]
+    standard_interest_df = standard_interest_df[["localTradedAt", "closePrice", "fluctuations"]]
 
     # 기존에 사용 중인 명칭으로 변경
-    price_df = price_df.rename(columns={
+    standard_interest_df = standard_interest_df.rename(columns={
         "localTradedAt": "date",
         "closePrice": "close",
         "fluctuations": "change"
     })
 
     # str -> int
-    price_df["close"] = (
-        price_df["close"]
+    standard_interest_df["close"] = (
+        standard_interest_df["close"]
         .str.replace(",", "", regex=False)
         .astype(float)
     )
 
     # str -. int
-    price_df["change"] = (
-        price_df["change"]
+    standard_interest_df["change"] = (
+        standard_interest_df["change"]
         .str.replace(",", "", regex=False)
         .astype(float)
     )
 
     # 사용자로부터 입력받은 데이터 입력
-    price_df["code"] = code
+    standard_interest_df["code"] = code
 
     # 데이터프레임 컬럼 순서 설정
-    price_df = price_df[
+    standard_interest_df = standard_interest_df[
         ["date", "code", "close", "change"]
     ]
 
     # 날짜 순으로 정렬 후 return
     return (
-        price_df
+        standard_interest_df
         .sort_values("date")
         .reset_index(drop=True)
     )
