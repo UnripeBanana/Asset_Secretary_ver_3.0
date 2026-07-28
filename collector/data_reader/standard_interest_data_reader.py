bfa7eb338c9274968dcac9b1e39b3bd17aa683fa
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-28 16:09:44 +0900

    Update standard_interest_data_reader.py

--- collector/data_reader/standard_interest_data_reader.py
+++ collector/data_reader/standard_interest_data_reader.py
@@ -6,7 +6,7 @@
 
 import requests
 import pandas as pd
-#from collector.data_processor.standard_interest_data_processor import standard_interest_data_processor
+from collector.data_processor.standard_interest_data_processor import standard_interest_data_processor
 
 def standard_interest_data_reader(start, end, code):
     start = pd.to_datetime(str(start))
