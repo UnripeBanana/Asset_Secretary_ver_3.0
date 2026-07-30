62e87a948278d477cf22602187922b141958628c
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-30 20:27:47 +0900

    Update double_price_data_reader.py

--- collector/data_reader/double_price_data_reader.py
+++ collector/data_reader/double_price_data_reader.py
@@ -1,2 +1,6 @@
-def double_price_data_reader():
-    
+from collector.data_reader.price_data_reader import price_data_reader
+
+def double_price_data_reader(start, end, code_1, code_2):
+    data_1 = price_data_reader(start, end, code_1)
+    data_2 = price_data_reader(start, end, code_2)
+    return data_1, data_2
