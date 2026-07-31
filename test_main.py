c74ccbba6d327f1a2320d1acce8b0637111c3af2
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-31 16:56:36 +0900

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -12,6 +12,8 @@ from collector.chart_maker.price_chart_maker import price_chart_maker
 from collector.chart_maker.standard_interest_chart_maker import standard_interest_chart_maker
 from collector.chart_maker.double_price_data_chart_maker import double_price_data_chart_maker
 
+from notion.main_page.chart_updator import main_page_chart_updator
+
 #-------------------------------------------
 # 작업 완료
 #-------------------------------------------
@@ -24,7 +26,7 @@ from collector.chart_maker.double_price_data_chart_maker import double_price_dat
 #index_candle_chart_maker(index_data_reader("2025-07-20", "2026-07-27", "KOSPI"))         
 # 입력 가능한 항목 : "KOSPI", "KOSDAQ", "KOSPI_200", "NASDAQ", "S&P_500", "Dow_Jones", "VIX"
 
-price_chart_maker(price_data_reader("2025-07-20", "2026-07-31", "USD-KRW"))              
+#price_chart_maker(price_data_reader("2025-07-20", "2026-07-31", "USD-KRW"))              
 # 입력 가능한 항목 : "US2Y", "US10Y", "US30Y", "KR3Y", "KR10Y", "KR30Y", "USD-KRW", "Dolar_Index", "USD-JPY", "USD-EUR", 
 #                   "KRX_Gold", "International_Gold", "Silver", "WTI_Crude_Oil", "Brent_Crude_Oil", "Natural_Gas", "Copper"
 
@@ -39,4 +41,4 @@ price_chart_maker(price_data_reader("2025-07-20", "2026-07-31", "USD-KRW"))
 #-------------------------------------------
 # 작업 중
 #-------------------------------------------
-
+main_page_chart_updator("3996e5aee08380d2a06be79472dc7a36")
