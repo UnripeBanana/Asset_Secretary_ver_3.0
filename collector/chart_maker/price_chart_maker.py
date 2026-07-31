fedfbdd99088d9a75f96aeba547665d8eec081dd
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-31 15:43:45 +0900

    Update price_chart_maker.py

--- collector/chart_maker/price_chart_maker.py
+++ collector/chart_maker/price_chart_maker.py
@@ -275,7 +275,7 @@ def price_chart_maker(price_df):
         ".DXY": "Dolar_Index",
         "USDJPY": "USD-JPY",
         "USDEUR": "USD-EUR",
-        "M04020000": "KRW_Gold",
+        "M04020000": "KRX_Gold",
         "GCcv1": "International_Gold",
         "SIcv1": "Silver",
         "CLcv1": "WTI_Crude_Oil",
