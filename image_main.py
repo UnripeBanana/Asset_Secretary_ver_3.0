34e33235cb6f9ac222fe5b50ba1cc60cca7469c2
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-08-01 16:28:38 +0900

    Update image_main.py

--- image_main.py
+++ image_main.py
@@ -12,37 +12,26 @@ from collector.chart_maker.price_chart_maker import price_chart_maker
 from collector.chart_maker.standard_interest_chart_maker import standard_interest_chart_maker
 from collector.chart_maker.double_price_data_chart_maker import double_price_data_chart_maker
 
-from datetime import datetime, timedelta
-
-today = datetime.now()
-today_str = datetime.now().strftime("%Y-%m-%d")
-
-three_month_ago = today - timedelta(days=91)
-three_month_ago_str = three_month_ago.strftime("%Y-%m-%d")
+from notion.index_callout.performer import index_callout_performer
 
 
+domestic_stock_chart_maker(domestic_stock_data_reader(90, "005930"), "삼성전자")
+domestic_stock_day_candle_chart_maker(domestic_stock_data_reader(90, "005930"), "삼성전자")
 
 
+for day in [90, 365, 365*3, 365*5, 365*10]:
+    for index in ["KOSPI", "KOSDAQ", "KOSPI_200", "NASDAQ", "S&P_500", "Dow_Jones", "VIX"]:
+        index_chart_maker(index_data_reader(day, index))
+        index_day_candle_chart_maker(index_data_reader(day, index))
 
-domestic_stock_chart_maker(domestic_stock_data_reader("2025-07-20", "2026-07-27", "005930"), "삼성전자")
-domestic_stock_day_candle_chart_maker(domestic_stock_data_reader("2025-07-20", "2026-07-27", "005930"), "삼성전자")
+    for price_1 in ["US2Y", "US10Y", "US30Y", "KR3Y", "KR10Y", "KR30Y", "USD-KRW", "Dolar_Index", "USD-JPY", "USD-EUR", "KRX_Gold", "International_Gold", "Silver", "WTI_Crude_Oil", "Brent_Crude_Oil", "Natural_Gas", "Copper"]:
+        price_chart_maker(price_data_reader(day, price_1))
+        for price_2 in ["US2Y", "US10Y", "US30Y", "KR3Y", "KR10Y", "KR30Y", "USD-KRW", "Dolar_Index", "USD-JPY", "USD-EUR", "KRX_Gold", "International_Gold", "Silver", "WTI_Crude_Oil", "Brent_Crude_Oil", "Natural_Gas", "Copper"]:
+            if price_1 == price_2: continue
+
+            double_price_data_chart_maker(double_price_data_reader(day, price_1, price_2))
+
+    for standard_interest in ["Korea_Rate", "Fed_Rate"]:
+        standard_interest_chart_maker(standard_interest_data_reader(day, standard_interest))
 
 
-
-for index in ["KOSPI", "KOSDAQ", "KOSPI_200", "NASDAQ", "S&P_500", "Dow_Jones", "VIX"]:
-    index_chart_maker(index_data_reader("2025-07-20", "2026-07-27", index))                
-# 입력 가능한 항목 : "KOSPI", "KOSDAQ", "KOSPI_200", "NASDAQ", "S&P_500", "Dow_Jones", "VIX"
-
-index_day_candle_chart_maker(index_data_reader("2025-07-20", "2026-07-27", "KOSPI"))         
-# 입력 가능한 항목 : "KOSPI", "KOSDAQ", "KOSPI_200", "NASDAQ", "S&P_500", "Dow_Jones", "VIX"
-
-price_chart_maker(price_data_reader("2025-07-20", "2026-07-31", "USD-KRW"))              
-# 입력 가능한 항목 : "US2Y", "US10Y", "US30Y", "KR3Y", "KR10Y", "KR30Y", "USD-KRW", "Dolar_Index", "USD-JPY", "USD-EUR", 
-#                   "KRX_Gold", "International_Gold", "Silver", "WTI_Crude_Oil", "Brent_Crude_Oil", "Natural_Gas", "Copper"
-
-standard_interest_chart_maker(standard_interest_data_reader("2000-04-27", "2026-07-27", "Fed_Rate"))
-# 입력 가능한 항목 : "Korea_Rate", "Fed_Rate"
-
-double_price_data_chart_maker(double_price_data_reader("2023-03-20", "2026-07-27", "Dolar_Index", "USD-KRW"))
-# 입력 가능한 항목 : "US2Y", "US10Y", "US30Y", "KR3Y", "KR10Y", "KR30Y", "USD-KRW", "Dolar_Index", "USD-JPY", "USD-EUR", 
-#                   "KRX_Gold", "International_Gold", "Silver", "WTI_Crude_Oil", "Brent_Crude_Oil", "Natural_Gas", "Copper"
