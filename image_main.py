68c2eb7136e7b6459d8ecc37ddc6c8145e8d279c
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-08-01 14:50:50 +0900

    Update image_main.py

--- image_main.py
+++ image_main.py
@@ -12,32 +12,24 @@ from collector.chart_maker.price_chart_maker import price_chart_maker
 from collector.chart_maker.standard_interest_chart_maker import standard_interest_chart_maker
 from collector.chart_maker.double_price_data_chart_maker import double_price_data_chart_maker
 
-#-------------------------------------------
-# 작업 완료
-#-------------------------------------------
-#domestic_stock_chart_maker(domestic_stock_data_reader("2025-07-20", "2026-07-27", "005930"), "삼성전자")
-#domestic_stock_candle_chart_maker(domestic_stock_data_reader("2025-07-20", "2026-07-27", "005930"), "삼성전자")
 
-#index_chart_maker(index_data_reader("2025-07-20", "2026-07-27", "KOSPI"))                
+domestic_stock_chart_maker(domestic_stock_data_reader("2025-07-20", "2026-07-27", "005930"), "삼성전자")
+domestic_stock_candle_chart_maker(domestic_stock_data_reader("2025-07-20", "2026-07-27", "005930"), "삼성전자")
+
+for index in ["KOSPI", "KOSDAQ", "KOSPI_200", "NASDAQ", "S&P_500", "Dow_Jones", "VIX"]:
+    index_chart_maker(index_data_reader("2025-07-20", "2026-07-27", index))                
 # 입력 가능한 항목 : "KOSPI", "KOSDAQ", "KOSPI_200", "NASDAQ", "S&P_500", "Dow_Jones", "VIX"
 
-#index_candle_chart_maker(index_data_reader("2025-07-20", "2026-07-27", "KOSPI"))         
+index_candle_chart_maker(index_data_reader("2025-07-20", "2026-07-27", "KOSPI"))         
 # 입력 가능한 항목 : "KOSPI", "KOSDAQ", "KOSPI_200", "NASDAQ", "S&P_500", "Dow_Jones", "VIX"
 
-#price_chart_maker(price_data_reader("2025-07-20", "2026-07-31", "USD-KRW"))              
+price_chart_maker(price_data_reader("2025-07-20", "2026-07-31", "USD-KRW"))              
 # 입력 가능한 항목 : "US2Y", "US10Y", "US30Y", "KR3Y", "KR10Y", "KR30Y", "USD-KRW", "Dolar_Index", "USD-JPY", "USD-EUR", 
 #                   "KRX_Gold", "International_Gold", "Silver", "WTI_Crude_Oil", "Brent_Crude_Oil", "Natural_Gas", "Copper"
 
-#standard_interest_chart_maker(standard_interest_data_reader("2000-04-27", "2026-07-27", "Fed_Rate"))
+standard_interest_chart_maker(standard_interest_data_reader("2000-04-27", "2026-07-27", "Fed_Rate"))
 # 입력 가능한 항목 : "Korea_Rate", "Fed_Rate"
 
-#double_price_data_chart_maker(double_price_data_reader("2023-03-20", "2026-07-27", "Dolar_Index", "USD-KRW"))
+double_price_data_chart_maker(double_price_data_reader("2023-03-20", "2026-07-27", "Dolar_Index", "USD-KRW"))
 # 입력 가능한 항목 : "US2Y", "US10Y", "US30Y", "KR3Y", "KR10Y", "KR30Y", "USD-KRW", "Dolar_Index", "USD-JPY", "USD-EUR", 
 #                   "KRX_Gold", "International_Gold", "Silver", "WTI_Crude_Oil", "Brent_Crude_Oil", "Natural_Gas", "Copper"
-
-#index_callout_performer()
-
-
-#-------------------------------------------
-# 작업 중
-#-------------------------------------------
